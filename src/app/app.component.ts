4782aee2442e3f7737d3503a44f15648f125f86f
cplan85 <carlos_planchart@live.com>
2022-06-07 23:33:13 +0200

    completed up to template filtering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  isActive = true;
+  isGreen = true;
   title = 'mi PrimerApp';
-  saludos='aqui esta mi saludo';
+  public name = 'Carlos';
+  public message = '';
+  saludos = 'aqui esta mi saludo';
+
+  save(e: Event) {
+    console.log(e);
+  }
+
+  onKeyUp(e: KeyboardEvent) {
+    if (e.keyCode === 13) {
+      console.log('Enter fue presionado');
+    }
+  }
 }
--- src/app/inline-example/inline-example.component.ts
+++ src/app/inline-example/inline-example.component.ts
@@ -1,21 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  //THIS COMPONENT WAS CREATED WITH "ng g c inline-example -s -t" 
+  //THIS COMPONENT WAS CREATED WITH "ng g c inline-example -s -t"
   selector: 'app-inline-example',
   template: `
-    <p>
-      inline-example works!
-    </p>
+    <p>inline-example works! {{ parentData }}</p>
+    <button (click)="fireEvent()">Send event</button>
   `,
-  styles: [
-  ]
+  styles: [],
 })
 export class InlineExampleComponent implements OnInit {
+  //you can use statement below
+  @Input() public parentData = 'Default Name';
+  @Output() public childEvent = new EventEmitter();
+  //or create alias is in below.
+  //@Input('parentData') public name;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  fireEvent() {
+    this.childEvent.emit('Hey this is Text from Child');
   }
-
 }
